767bf03aac1d2ed22d75468a4995a56d929bd6f6
iynsys <irgiyansy@gmail.com>
2022-04-18 14:32:11 +0700

    test

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,6 +2,10 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+function __fish_command_not_found_handler --on-event fish_command_not_found
+  echo "Oops, no '$argv', try again!"
+  return
+end
 
 alias ll='exa -l --color always --icons -a -s type'
 alias l='exa -l --color always --icons -a -s type'
